88d80d02e5d35c559f7499999a424a4d91206b5c
“jayaraman-B” <bjayaraman96@gmail.com>
2020-06-02 05:23:08 +0530

    Added the primeter function

--- math_func.py
+++ math_func.py
@@ -3,4 +3,9 @@ r=int(input("Enter the Radius of the circle:"))
 def area(r):
     area=round(3.14*r*r,2)
     print("Area of the circle is ",area)
+
+def perimeter(r):
+    perimeter=round(2*3.14*r,2)
+    print("Area of the perimeter is",perimeter)
 area(r)
+perimeter(r)
