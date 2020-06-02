29eb82a0c17093293721ea9024f2336c040f38ae
“jayaraman-B” <bjayaraman96@gmail.com>
2020-06-02 05:33:47 +0530

    Added the area of cylinder

--- math_func.py
+++ math_func.py
@@ -1,4 +1,5 @@
 r=int(input("Enter the Radius of the circle:"))
+h=int(input("Enter the height of the cylindr:"))
 
 def area(r):
     area=round(3.14*r*r,2)
@@ -7,5 +8,12 @@ def area(r):
 def perimeter(r):
     perimeter=round(2*3.14*r,2)
     print("Area of the perimeter is",perimeter)
+
+def cylinder_area(r,h):
+    area=round(3.14*r*r*h,2)
+    print("Area of the cylinder is ",area)
+
+    
 area(r)
 perimeter(r)
+cylinder_area(r,h)
